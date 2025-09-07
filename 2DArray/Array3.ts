f6ed890039dacbcffa9d057d4f1a3269985b0265
ashu130698 <ashushukla299@gmail.com>
2025-09-07 23:27:12 +0530

    add line

---
+++ 2DArray/Array3.ts
@@ -0,0 +1,13 @@
+let arr = [
+    [2, 3, 4],
+    [5, 6, 7],
+    [8, 9, 1]
+    
+];
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+        console.log(arr[i][j]);
+        
+    }
+}
