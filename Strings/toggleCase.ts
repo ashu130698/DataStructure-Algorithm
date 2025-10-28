33e4435bd07c718c8fe868a8e0aff9ccc390b0df
ashu130698 <ashushukla299@gmail.com>
2025-10-28 23:57:19 +0530

    Palindrome case

--- Strings/toggleCase.ts
+++ Strings/toggleCase.ts
@@ -2,13 +2,13 @@ function toggleCase(s: string): string {
     let ans = "";
 
     for (let i = 0; i < s.length; i++){
-        let ascii = s.charCodeAt(i);
+        let ascii = s.charCodeAt(i);  //converting char to ascii
         if (ascii < 97) {
             ascii += 32;
         } else {
             ascii -= 32;
         }
-        ans += String.fromCharCode(ascii);
+        ans += String.fromCharCode(ascii);   //convert ascii to char
     }
     return ans;
 }
