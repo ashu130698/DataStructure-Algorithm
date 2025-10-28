453f130ca0f8d7e8fdda3dede21109b87b907a25
ashu130698 <ashushukla299@gmail.com>
2025-10-28 23:39:49 +0530

    toggle String case

---
+++ Strings/toggleCase.ts
@@ -0,0 +1,16 @@
+function toggleCase(s: string): string {
+    let ans = "";
+
+    for (let i = 0; i < s.length; i++){
+        let ascii = s.charCodeAt(i);
+        if (ascii < 97) {
+            ascii += 32;
+        } else {
+            ascii -= 32;
+        }
+        ans += String.fromCharCode(ascii);
+    }
+    return ans;
+}
+
+console.log(toggleCase("aBcDeF"));
