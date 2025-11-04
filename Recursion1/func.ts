cd661392af39e2e36ff9e5eda08e93677c645ed1
ashu130698 <ashushukla299@gmail.com>
2025-11-04 23:30:17 +0530

    p

--- Recursion1/func.ts
+++ Recursion1/func.ts
@@ -1,8 +1,17 @@
-function func(n: number): void{
-    if (n == 0) return;
-    console.log(n);
-    func(n - 1)
-    console.log(n);  
+// function func(n: number): void{
+//     if (n == 0) return;
+//     console.log(n);
+//     func(n - 1)
+//     console.log(n);
+// }
+
+// func(5);
+
+function func(n: number): void {
+  if (n == 101) return;
+  console.log(n);
+  func(n + 1);
+  console.log(n);
 }
 
-func(5);
+func(95);
